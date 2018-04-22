7f88af6a338b21198685db4b97e208b7cb2ef761
shadow6379 <wuchg6@mail2.sysu.edu.cn>
2018-04-22 22:20:08 +0800

    initial api design

---
+++ web_server/user_app/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+
+
+def registry(request):
+    pass
+
+
+def login(request):
+    pass
+
+
+def logout(request):
+    pass
+
+
+def category(request, cid, begin, end):
+    pass
+
+
+def detail(request, bid):
+    pass
+
+
+def collect_book(request):
+    pass
+
+
+def subscribe_book(request):
+    pass
+
+
+def star_book(request):
+    pass
+
+
+class CommentSection(View):
+    # add auth decorator here
+    def dispatch(self, request, *args, **kwargs):
+        return super(CommentSection, self).dispatch(request, *args, **kwargs)
+
+    @staticmethod
+    def get(request):
+        pass
+
+    @staticmethod
+    def post(request):
+        pass
+
+
+class UserProfile(View):
+    # add auth decorator here
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserProfile, self).dispatch(request, *args, **kwargs)
+
+    @staticmethod
+    def get(request):
+        pass
+
+    @staticmethod
+    def post(request):
+        pass
+
+
+def retrieve(request):
+    pass
--- web_server/web_server/settings.py
+++ web_server/web_server/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user_app',
+    'manager_app',
 ]
 
 MIDDLEWARE = [
