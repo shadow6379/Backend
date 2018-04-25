96eec665de4d68fbfcb281ecd2b6d4eabb4d72cb
shadow6379 <wuchg6@mail2.sysu.edu.cn>
2018-04-25 17:13:42 +0800

    db table design

--- web_server/manager_app/admin.py
+++ web_server/manager_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from manager_app import models
 
 # Register your models here.
+
+
+class ManagerInfoAdmin(admin.ModelAdmin):
+    list_display = ('username', 'password', 'email', 'gender', 'phone', 'other')
+
+
+admin.site.register(models.ManagerInfo, ManagerInfoAdmin)
--- web_server/manager_app/models.py
+++ web_server/manager_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class ManagerInfo(models.Model):
+    username = models.CharField(max_length=20, verbose_name=u'用户名', unique=True)
+    password = models.CharField(max_length=32, verbose_name=u'密码')
+    email = models.EmailField(verbose_name=u'邮箱')
+    gender_choices = (
+        (0, u'保密'),
+        (1, u'男'),
+        (2, u'女')
+    )
+    gender = models.IntegerField(choices=gender_choices, default=0, verbose_name=u'性别')
+    phone = models.CharField(max_length=20, null=True, blank=True, verbose_name='电话号码')
+    other = models.TextField(null=True, blank=True, verbose_name=u'其它')
+
+    class Meta:
+        verbose_name = u'管理员'
+        verbose_name_plural = u'管理员信息'
+
