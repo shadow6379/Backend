5358e62e59c9a237d7dbd76306eb338a264023c0
shadow6379 <wuchg6@mail2.sysu.edu.cn>
2018-04-27 15:36:27 +0800

    add mgr auth test

--- web_server/manager_app/tests.py
+++ web_server/manager_app/tests.py
@@ -1,3 +1,49 @@
+import json
+
 from django.test import TestCase
+from django.test import Client
+
+from manager_app import models
 
 # Create your tests here.
+
+
+class LoginTestCase(TestCase):
+    def setUp(self):
+        models.ManagerInfo.objects.create(
+            username='test',
+            password='123456',
+            email='test@163.com',
+        )
+
+    def test_login(self):
+        client = Client()
+
+        request = {
+            'username': 'test',
+            'password': '123456',
+        }
+        response = client.post('/manager_app/login/', request)
+        self.assertEqual(json.loads(response.content.decode())['status'], 'success')
+
+        request = {
+            'username': 'test',
+            'password': '123',
+        }
+        response = client.post('/manager_app/login/', request)
+        self.assertEqual(json.loads(response.content.decode())['status'], 'failure')
+
+        request = {
+            'username': 'te',
+            'password': '123456',
+        }
+        response = client.post('/manager_app/login/', request)
+        self.assertEqual(json.loads(response.content.decode())['status'], 'failure')
+
+        request = {
+            'username': 'te',
+            'password': '123',
+        }
+        response = client.post('/manager_app/login/', request)
+        self.assertEqual(json.loads(response.content.decode())['status'], 'failure')
+
