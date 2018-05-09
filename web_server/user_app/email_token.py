7a7540c0f0a47e708fa1f334ce049c50aa62a240
xiejx <15975129956@139.com>
2018-05-09 15:20:11 +0800

    Update

--- web_server/user_app/email_token.py
+++ web_server/user_app/email_token.py
@@ -1,17 +1,25 @@
 from itsdangerous import URLSafeTimedSerializer as utsr
-from web_server import settings
+from web_server.settings import SECRET_KEY
 import base64
 
 
+# Token class, using for creating token
 class Token:
+    # initialize the class
+    # using base64 to  encrypt the security_key
     def __init__(self, security_key):
         self.security_key = security_key
         self.salt = base64.encodebytes(security_key.encode('utf-8')).decode('utf-8')
 
+    # serializing the itsdangerous, which contains the timestamp
     def generate_validate_token(self, username):
         serializer = utsr(self.security_key)
+        # convert the serializer to the json form
         return serializer.dumps(username, self.salt)
 
+    # validating the token
+    # the period of validity is expiration second(s), default = 3600s
+    # return the username if it is in period
     def confirm_validate_token(self, token, expiration=3600):
         serializer = utsr(self.security_key)
         return serializer.loads(
@@ -20,10 +28,11 @@ class Token:
             max_age=expiration
         )
 
+    # whether it is in period, it returns a username
     def remove_validate_token(self, token):
         serializer = utsr(self.security_key)
-        # print serializer.loads(token, salt=self.salt)
         return serializer.loads(token, salt=self.salt)
 
 
-token_confirm = Token(settings.SECRET_KEY)
+# define it the global variable
+token_confirm = Token(SECRET_KEY)
